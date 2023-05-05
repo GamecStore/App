06f50cf7eab84ad45cba1c5785434066db43f375
Hoss <geebi2003@gmail.com>
2023-05-05 22:25:18 +0300

    Get -> post

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 const { testController } = require('../controllers/UserController.js');
 
 module.exports = (app) => {
-    app.get('/test', testController);
+    app.post('/test', testController);
 }
