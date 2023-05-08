30a97ffb94467a0def521ab586f8885184559975
Adam Loay <adamlo2ay@gmail.com>
2023-05-09 02:34:08 +0300

    Made header it's own file and added ejs

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ const config = require('./config.json');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 require("colors");
 
 const app = express();
 const port = config.port || 3000;
-
+app.set('view engine', 'ejs')
 app.use(cors());
 app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'));
 
 mongoose.connect(config.mongoURI);
 const connection = mongoose.connection;
@@ -22,6 +25,11 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.sendStatus(200);
+});
+
+
+app.get('/gamepage', (req, res) => {
+    res.render('gamePage');
 });
 
 require('./routes/ping')(app);
