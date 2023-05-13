9ad694bc52e3f2c0f4fd1c57300f12b45f7f40df
Adam Loay <adamlo2ay@gmail.com>
2023-05-13 20:57:24 +0300

    added an object to test dynamic data with ejs

--- src/routes/gamepage.js
+++ src/routes/gamepage.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+
+
+
 router.get('gamepage', (req, res) => {
-    res.render('pages/gamePage')
+
+    games: [
+        {
+            name: 'God Of war Ragnarock',
+            description: 'Journey to dangerous and stunning landscapes Embark on an epic and heartfelt journey as Kratos and Atreus struggle with holding on andletting go.',
+            developer: 'Sony Santa Monica ',
+            publisher: 'Sony Entertainment',
+            price: [59.99, 69.99, 79.99],
+            editionsDesc: ['God of War Ragnarök ps4'],
+            sliderImgs: ''
+        }
+    ]
+
+
+    res.render('pages/gamePage', { games })
 })
 
 module.exports = router
