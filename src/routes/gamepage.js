0cb771e52bdfe2571a98ed3e5514c8cbad8b55c4
Adam Loay <adamlo2ay@gmail.com>
2023-05-15 23:03:26 +0300

    Made images and carasol dynamic

--- src/routes/gamepage.js
+++ src/routes/gamepage.js
@@ -16,10 +16,11 @@ router.get('/gamepage/:id', (req, res) => {
             publisher: 'Sony Entertainment',
             price: [59.99, 69.99, 79.99],
             editionsDesc: ['God of War Ragnarök ps4'],
-            sliderImgs: ['GOWR1.webp', 'GOWR2.webp', 'GOWR3.webp']
+            sliderImgs: ['GOWR1.webp', 'GOWR2.webp', 'GOWR3.webp', 'GOWR3.webp', "GOWR2.webp"],
+            sideImg: 'GOWR1.webp'
         },
         {
-            id: 1,
+            id: 2,
             name: 'Jedi Fallen Order',
             sideDescription: 'Journey to dangerous and stunning landscapes Embark on an epic and heartfelt journey as Kratos and Atreus struggle with holding on and letting go.',
             mainDescription: 'From Santa Monica Studio comes the sequelto the critically acclaimed God of War (2018). Fimbulwinter is well underway. Kratos and Atreus must journey to each of the Nine Realms in search of answers as Asgardian forces prepare for a prophesied battle that will end the world.Along the way they will explore stunning, mythical landscapes, and face fearsome enemies in the form of Norse gods and monsters. The threat of Ragnarök grows ever closer.Kratos and Atreus must choose between their own safety and the safety of the realms. ',
@@ -27,7 +28,7 @@ router.get('/gamepage/:id', (req, res) => {
             publisher: 'Sony Entertainment',
             price: [59.99, 69.99, 79.99],
             editionsDesc: ['God of War Ragnarök ps4'],
-            sliderImgs: ['GOWR1.webp', 'GOWR2.webp', 'GOWR3.webp']
+            sliderImgs: ['GOWR1.webp', 'GOWR2.webp', 'GOWR3.webp',]
         }
     ]
     game = games.find(game => game.id === parseInt(req.params.id))
