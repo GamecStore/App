1e31fbb1ad9c7578f3754aba6e5728cd42bcf06c
Adam <adamlo2ay@gmail.com>
2023-05-22 18:01:31 +0300

    updated schema

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const User = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     gender: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    confirmpassword: { type: String, required: true},
+    confirmpassword: { type: String, required: true },
     dateOfBirth: { type: Date },
     role: { type: String, default: 'user' },
     // created: { type: Date, default: Date.now }
