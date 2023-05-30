7f5e863650227405e4699f42e035bbf8e447daf9
Adam <adamlo2ay@gmail.com>
2023-05-30 18:48:44 +0300

    added ajax to make sure that the username is not already taken

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -94,6 +94,7 @@ const gamepage = (req, res) => {
     })
 }
 
+
 module.exports = {
     createGame,
     getAllGames,
--- src/public/Js/ajax.js
+++ src/public/Js/ajax.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
     $("#name").on('keyup', function (e) {
         e.preventDefault();
         var data = $('#name').val();
+        console.log("in ajax")
         $.ajax({
-            url: '/users/checkName',
+            url: '/checkName',
             method: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({ UserName: data }),
+            data: JSON.stringify({ name: data }),
             success: function (response) {
                 $('#result').html('UserName is ' + response);
 
