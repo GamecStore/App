5ddb887b8859b2c5fdb5a9320e183668af8a4398
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2023-06-04 16:20:22 +0300

    files

--- src/routes/allGames.js
+++ src/routes/allGames.js
@@ -1,7 +1,41 @@
 const express = require("express")
 const router = express.Router()
 const AllGamesContoller = require('../controllers/allGamesContoller')
-const upload = require('../middleware/upload')
+const path = require('path');
+const multer = require('multer');
+
+//image validations
+const storage = multer.diskStorage({
+    distination: function(req,file,cb) {
+        cb(null,'../public/images/gameimages')  //location where the file will be saved
+    },
+    filename: function(req,file,cb) {  //rename the file withtimestamp and extension (always unique)
+        const ext = path.extname(file.originalname);
+        cb(null,Date.now() + ext);
+    }
+});
+
+// image extensions
+const upload = multer({
+    storage:storage,
+    fileFilter:function(req,file,callback){
+        if(
+            file.mimetype == 'image/jpeg'||
+            file.mimetype == 'image/jpg'||
+            file.mimetype == 'image/png'
+        ){
+            callback(null,true);
+        }
+        else{
+            console.log("Only jpg & png files are supported!");
+            callback(null,true);
+        }
+    },
+    limits:{
+        fileSize:1024 * 1024 * 2 
+    }
+});
+
 
 router.get('/', AllGamesContoller.allGames_get)
 router.post('/', upload.single('poster'), AllGamesContoller.allGames_post)
