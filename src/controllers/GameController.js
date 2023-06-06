681e787184dac7464ae8665fa8190e54aaab79cd
Adam Loay <adamlo2ay@gmail.com>
2023-06-07 00:19:53 +0300

    removed unused code

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -2,90 +2,10 @@ const Game = require('../models/Game.js')
 const user = require('../models/User.js')
 const games = require('../models/Game')
 
-const createGame = async (req, res) => {
-    try {
-        const game = new Game(req.body);
-        await game.save();
-        res.status(201).json(game);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-};
 
-const getAllGames = async (req, res) => {
-    try {
-        const games = await Game.find();
-        res.status(200).json(games);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
 
-const getGameById = async (req, res) => {
-    try {
-        const game = await Game.findById(req.params.id);
-        if (!game) {
-            return res.status(404).json({ message: 'Game not found' });
-        }
-        res.status(200).json(game);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
 
-const updateGameById = async (req, res) => {
-    try {
-        const game = await Game.findById(req.params.id);
-        if (!game) {
-            return res.status(404).json({ message: 'Game not found' });
-        }
-        Object.keys(req.body).forEach((key) => {
-            game[key] = req.body[key];
-        });
-        await game.save();
-        res.status(200).json(game);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
 
-const deleteGameById = async (req, res) => {
-    try {
-        const game = await Game.findById(req.params.id);
-        if (!game) {
-            return res.status(404).json({ message: 'Game not found' });
-        }
-        await game.remove();
-        res.status(200).json({ message: 'Game deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-const filterGames = async (req, res) => {
-    try {
-        const filter = {};
-        if (req.query.name) {
-            filter.name = { $regex: req.query.name, $options: 'i' };
-        }
-        if (req.query.developer) {
-            filter.developer = { $regex: req.query.developer, $options: 'i' };
-        }
-        if (req.query.publisher) {
-            filter.publisher = { $regex: req.query.publisher, $options: 'i' };
-        }
-        if (req.query.genre) {
-            filter.genre = { $in: req.query.genre };
-        }
-        if (req.query.price) {
-            filter.price = { $lte: req.query.price };
-        }
-        const games = await Game.find(filter);
-        res.status(200).json(games);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
 
 const gamepage = (req, res) => {
     const id = req.params.id;
@@ -145,12 +65,8 @@ const allGames_post = async (req, res) => {
 
 
 module.exports = {
-    createGame,
-    getAllGames,
-    getGameById,
-    updateGameById,
-    deleteGameById,
-    filterGames,
+
+
     gamepage,
     searchGame,
     allGames_get,
