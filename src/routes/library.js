8a1a2953595cfff42aeadabdaede451b2eaa27d1
Adam Loay <adamlo2ay@gmail.com>
2023-06-07 12:20:40 +0300

    another one

--- src/routes/library.js
+++ src/routes/library.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const UserController = require('../controllers/UserController');
 
-router.get('/', (req, res) => {
-    res.render('pages/library', { user: req.session.user })
-})
+router.get('/', UserController.library)
 
 module.exports = router
